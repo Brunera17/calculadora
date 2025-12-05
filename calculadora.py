36a83792bcec1df189632777f18894db397358d8
Bruno David Martins <dbrunobruno369@gmail.com>
2025-12-05 12:34:24 -0300

    Corrigindo o commit desatualizado

--- calculadora.py
+++ calculadora.py
@@ -40,18 +40,3 @@ def calculo(operacao, num1, num2):
         print("Operação inválida")
         operacao = 5
         return operacao
-
-
-operacao, num1, num2 = operacoes()
-
-while operacao != 0:
-    try:
-        resultado, texto = calculo(operacao, num1, num2)
-    except ZeroDivisionError:
-        print("Erro: Divisão por zero não é permitida.")
-    if operacao != 5:
-        print("---------------------")
-        print(f"A {texto} dos numeros é igual a: {resultado}")
-        print("---------------------")
-    operacao, num1, num2 = operacoes()
-print("Até mais!")
